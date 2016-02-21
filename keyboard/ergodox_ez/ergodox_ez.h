da82e5c9018dc07fb2e348a082bc92d24678d91d
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-02-21 12:49:52 +0100

    Decreased LED brightness.

--- keyboard/ergodox_ez/ergodox_ez.h
+++ keyboard/ergodox_ez/ergodox_ez.h
@@ -34,8 +34,8 @@ void ergodox_blink_all_leds(void);
 uint8_t init_mcp23018(void);
 uint8_t ergodox_left_leds_update(void);
 
-#define LED_BRIGHTNESS_LO       15
-#define LED_BRIGHTNESS_HI       255
+#define LED_BRIGHTNESS_LO       1
+#define LED_BRIGHTNESS_HI       2
 
 
 inline void ergodox_board_led_on(void)      { DDRD |=  (1<<6); PORTD |=  (1<<6); }
