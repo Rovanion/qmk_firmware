78f0f5f6d017b6a1628149cc26092d1778bfe7ee
Rovanion Luckey <rovanion.luckey@gmail.com>
2017-01-24 16:30:09 +0100

    Added plover layout for stenography.

--- keyboards/ergodox/keymaps/rovanion/keymap.c
+++ keyboards/ergodox/keymaps/rovanion/keymap.c
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// Gaming layout
+
 KEYMAP(// Left hand.
 			 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -181,6 +181,48 @@ KEYMAP(// Left hand.
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
+
+/* Keymap 4: Steno for Plover from https://github.com/shayneholmes/tmk_keyboard/commit/11290f8489013018f778627db725160c745e75bd
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |   8  |   9  |  0   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   q  |   w  |   e  |   r  |   t  |------|           |------|  y   |  u   |   i  |   o  |  p   |   [    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   a  |   s  |   d  |   f  |   g  |      |           |      |  h   |  j   |   k  |   l  |  ;   |   '    |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |   c  |   v  |------|       |------|  n   |  m   |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+
+[PLVR] = KEYMAP(  // layout: layer 4: Steno for Plover
+        // Left hand.
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_1 ,   KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
+        KC_TRNS,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        KC_TRNS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TRNS,
+        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_FN4, KC_TRNS,
+                                           KC_TRNS,
+                                 KC_C,   KC_V,   KC_TRNS,
+        // Right hand.
+        KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,
+        KC_TRNS,   KC_6,    KC_7,    KC_8,   KC_9,   KC_0,    KC_TRNS,
+                   KC_Y,    KC_U,    KC_I,   KC_O,   KC_P,    KC_LBRC,
+        KC_TRNS,   KC_H,    KC_J,    KC_K,   KC_L,   KC_SCLN, KC_QUOT,
+                   KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,
+        KC_TRNS, KC_TRNS,
+        KC_TRNS,
+        KC_TRNS, KC_N,  KC_M),
 };
 
 
