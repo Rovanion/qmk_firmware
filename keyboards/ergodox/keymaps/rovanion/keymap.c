12b3113057660e5fe4bff5d0ab8662fe4363e705
Rovanion Luckey <rovanion.luckey@gmail.com>
2017-01-24 16:29:29 +0100

    Stylistic changes to the rovanion keymap file.

--- keyboards/ergodox/keymaps/rovanion/keymap.c
+++ keyboards/ergodox/keymaps/rovanion/keymap.c
@@ -7,6 +7,7 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 #define GAME 3 // Gaming keys
+#define PLVR 4 // Stenography through plover
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -33,34 +34,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
-        // left hand
-        KC_ESC,    KC_1,       KC_2,          KC_3,     KC_4,   KC_5,   KC_F5,
-        KC_TAB,    KC_Q,       KC_W,          KC_E,     KC_R,   KC_T,   MO(1),
-        KC_LGUI,   KC_A,       KC_S,          KC_D,     KC_F,   KC_G,
-        KC_LSFT,   KC_Z,       KC_X,          KC_C,     KC_V,   KC_B,   S(KC_INS),
-        KC_LCTL,   KC_QUOT,    KC_LEFT,       KC_RGHT,  KC_LALT,
-                                              ALT_T(KC_APP), KC_HOME,
+        // Left hand.
+        KC_ESC,    KC_1,       KC_2,     KC_3,     KC_4,   KC_5,   KC_F5,
+        KC_TAB,    KC_Q,       KC_W,     KC_E,     KC_R,   KC_T,   MO(1),
+        KC_LGUI,   KC_A,       KC_S,     KC_D,     KC_F,   KC_G,
+        KC_LSFT,   KC_Z,       KC_X,     KC_C,     KC_V,   KC_B,   S(KC_INS),
+        KC_LCTL,   KC_QUOT,    KC_LEFT,  KC_RGHT,  KC_LALT,
+                                         ALT_T(KC_APP), KC_HOME,
                                                               KC_END,
                     MT(MOD_LCTL,KC_SPC),MT(MOD_LALT,KC_ENT),  KC_GRV,
 
-	// right hand
-	KC_DEL,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-	TG(GAME),     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
-	              KC_H,   KC_J,   KC_K,   KC_L,   NO_AE,            NO_OSLH,
-	MEH_T(KC_NO), KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   NO_APOS,
-	KC_RALT,      KC_RBRC,KC_UP,  KC_DOWN,          KC_FN1,
-	MO(2),        CTL_T(KC_ESC),
-	KC_PGUP,
-	KC_PGDN,      LT(SYMB,KC_TAB), MT(MOD_LSFT,KC_BSPC)
-    ),
+        // Right hand.
+        KC_DEL,       KC_6,    KC_7,     KC_8,     KC_9,    KC_0,            KC_MINS,
+        TG(GAME),     KC_Y,    KC_U,     KC_I,     KC_O,    KC_P,            KC_LBRC,
+                      KC_H,    KC_J,     KC_K,     KC_L,    NO_AE,           NO_OSLH,
+        TG(PLVR),     KC_N,    KC_M,     KC_COMM,  KC_DOT,  CTL_T(KC_SLSH),  NO_APOS,
+                      KC_RALT, KC_RBRC,  KC_UP,    KC_DOWN, KC_FN1,
+        MO(2),        CTL_T(KC_ESC),
+        KC_PGUP,
+        KC_PGDN,      LT(SYMB,KC_TAB), MT(MOD_LSFT,KC_BSPC)),
 
 
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |  F11   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |  F12   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   ;  |   /  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -162,8 +162,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 // Gaming layout
-KEYMAP(
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+KEYMAP(// Left hand.
+			 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -171,7 +171,7 @@ KEYMAP(
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
                                   KC_SPC,  KC_ENT,  KC_TRNS,
-    // right hand
+       // Right hand.
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -232,4 +232,3 @@ void matrix_scan_user(void) {
     break;
   }
 };
-
